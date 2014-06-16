b548dc2b2c41674e4a52259a3e4d99c891044863
pandevirus@gmail.com <pandevirus@gmail.com>
2014-06-16 10:02:51 +0800

    Adding some comments to word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -11,6 +11,9 @@ class Word2Vec():
         `min_count` = ignore all words with count less than this threshold
         `model` 0 for CBOW, 1 for Skip-Gram
         `optimization` 0 for hierarchical softmax, 1 for negative sampling
+        `alpha` is the learning rate
+        `negative` is the number of negative samples used in negative sampling
+        `cbow_mean` indicates whether average projection vector in CBOW
         '''
         self.sentences = sentences
         self.layer_size = layer_size
@@ -34,34 +37,50 @@ class Word2Vec():
                 for pos, word in enumerate(sentence):
                     if word is None:
                         continue
+                    # Using words in the random window of the given words to predict this word
                     random_window = random.randint(model.window)
                     start = max(0, pos - random_window)
                     context_indices = [self.index[w] for w in (sentence[start: pos + random_window]) if(w is not None and w != word)]
+                    # Projection
                     l1 = np_sum(self.hidden[context_indices], axis = 0)
+                    # Mean
                     if self.cbow_mean and context_indices:
                         l1 /= len(context_indices)
                     neu1e = zeros(l1.shape)
+                    # negative sampling
                     if self.optimization == 1:
-                        # negative sampling
+                        # Using the current word as positive samples and #negative other random words as negative samples
                         labels = zeros(self.negative + 1)
                         labels[0] = 1.
+                        # The indices of words using as targets, including current word and #negative other random words
                         word_indices = [self.index[word]]
                         while len(word_indices) < self.negative + 1:
                             index_w = random.randint(self.n_word)
                             if index_w != self.index[word]:
                                 word_indices.append(index_w)
+                        # The weight from hidden layer to output layer
                         l2b = self.w_hidden_2_output[word_indices]
+                        # Hidden layer to output layer
                         fb = 1. / (1. + exp(-dot(l1, l2b.T)))
+                        # Error * learning rate
                         gb = (labels - fb) * self.alpha
+                        # Update weights
                         self.w_hidden_2_output[word_indices] += outer(gb, l1)
                         neu1e += dot(gb, l2b)
-
+                    # Update word vectors
                     self.hidden[context_indices] += neu1e
 
         elif self.model == 1:
             pass
 
     def build_vocab(self, sentences):
+        '''
+        `sentences` is a list of sentence, a sentence is a list of words
+        |self.word| is a list of valid words
+        |self.index| is a dict, word as key and index as value
+        |self.n_word| is the number of valid words
+        This function build the vocabulary of all valid words, a word is valid if its count is higher than $min_count
+        '''
         d_word = {}
         for sentence in sentences:
             for word in sentence:
